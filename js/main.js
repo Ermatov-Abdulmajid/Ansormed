4a2a778580ece5c64f130fe399bdc05fcff0a8d3
Ermatov-Abdulmajid <ermatovabdumajid23@gmail.com>
2024-06-11 07:40:47 +0500

    Add site responsive styles

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const elSiteHeader = document.querySelector(".site-header");
+const elSiteHeaderToggleButton = elSiteHeader.querySelector(
+  ".site-header-toggle-button"
+);
+
+if (elSiteHeaderToggleButton) {
+  elSiteHeaderToggleButton.addEventListener("click", function () {
+    elSiteHeader.classList.toggle("site-header-open");
+  });
+}
